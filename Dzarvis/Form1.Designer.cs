aeee49fcc5d4e5d2102508009e2ed9640359eda7
krapi <krapi@DESKTOP-08V2U59>
2022-12-30 11:46:47 +0200

    Добавьте файлы проекта.

---
+++ Dzarvis/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Dzarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(255, 213);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(109, 39);
+            this.label.TabIndex = 0;
+            this.label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "label";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Dzarvis/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+
+
+
+
+namespace Dzarvis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static Label l;
+
+        public static void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Confidence > 0.8) l.Text = e.Result.Text;
+            
+        }
+
+        public void Form1_Shown(object sender, EventArgs e)
+        {
+            
+            l = label;
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-Ru");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Sre_SpeechRecognized);
+
+
+            Choices Command = new Choices();
+            Command.Add(new string[] { "Привет" });
+
+
+            GrammarBuilder gb = new GrammarBuilder();
+            //gb.Culture = ci;
+            gb.Append(Command);
+
+
+            if(label.Text == "Привет")
+            {
+                //Тут будет аудио
+            }
+
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+
+    }
+
+}
